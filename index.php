d30fe5bf3fbcc1b9bced8c3b625863b9c5ed2c4a
Rostislav Khaniukov <rostyslav.khanyukov@ms.sumdu.edu.ua>
2019-04-01 21:08:17 +0300

    Drop

--- index.php
+++ index.php
@@ -5,12 +5,6 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use AirSlate\Releaser\Client;
 
-/*$client = new Client([
-    'owner' => 'airslateinc',
-    'repo' => 'mysql-prefill-addon',
-    'token' => '17b7a4f5e1408effafa7a48e6277745eefb150d9',
-]);*/
-
 $client = new Client([
     'owner' => 'rostyslavkhaniukov',
     'repo' => 'addon-release-tool',
