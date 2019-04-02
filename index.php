dd7d55809bad6856d618575af9568276a6ec178c
Rostislav Khaniukov <rostyslav.khanyukov@ms.sumdu.edu.ua>
2019-04-02 20:28:19 +0300

    Extend lib

--- index.php
+++ index.php
@@ -1,19 +1,68 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use AirSlate\Releaser\Client;
 
 $client = new Client([
-    'owner' => 'rostyslavkhaniukov',
-    'repo' => 'addon-release-tool',
-    'token' => '17b7a4f5e1408effafa7a48e6277745eefb150d9',
+    'owner' => 'airslateinc',
+    'token' => '',
 ]);
 
-try {
-    $webhooks = $client->labels()->all('addon-release-tool');
-} catch (DomainException $exception) {
-    var_dump($exception->getMessage());
+function addLabel(Client $client, $newLabel)
+{
+    $addons = [
+        'google-spreadsheets-postfinish-addon',
+        'google-spreadsheets-watcher-addon',
+        'webhook-addon',
+        'notification-addon',
+        'prefill-from-source-addons',
+        'google-spreadsheets-duplex-addon',
+        'jira-addon',
+        'revoke-access-addon',
+        'dropdown-options-prefill-addon',
+        'slate-prefill-addon',
+        'document-prefill-addon',
+        'slack-notifier-addon',
+        'send-slate-addon',
+        'change-order-addon',
+        'roles-management-addon',
+        'roles-users-management-addon',
+        'packet-delete-addon',
+        'set-packet-name-addon',
+        'weekly-reminder-addon',
+        'google-calendar-addon',
+        'lock-fields-addon',
+        'sms-notifier-addon',
+    ];
+
+    foreach ($addons as $addon) {
+        $labels = $client->labels()->all($addon);
+        $found = false;
+        foreach ($labels as $label) {
+            if ($label->getName() === $newLabel['name']) {
+                $found = true;
+                if ($label->getColor() !== $newLabel['color']
+                    || $label->getDescription() !== $newLabel['description']) {
+                    $client->labels()->update($addon, $label->getName(), $newLabel['color'], $newLabel['description']);
+                }
+            }
+        }
+        if (!$found) {
+            $client->labels()->create($addon, $newLabel['name'], $newLabel['color'], $newLabel['description']);
+        }
+    }
 }
-var_dump($webhooks);
+
+addLabel($client, [
+    'name' => 'approved',
+    'color' => '008672',
+    'description' => 'Approved by applications and platform teams',
+]);
+
+addLabel($client, [
+    'name' => 'waiting for platform review',
+    'color' => '1d76db',
+    'description' => 'Approved by applications team and waiting for platform team',
+]);
