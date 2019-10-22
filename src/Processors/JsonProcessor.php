c17d4d2d35e3e6225bc61218b6af5bf0f4413bf3
Rost Khaniukov <khaniukov.rostyslav@pdffiller.team>
2019-10-22 14:38:38 +0300

    a

--- src/Processors/JsonProcessor.php
+++ src/Processors/JsonProcessor.php
@@ -24,4 +24,39 @@ class JsonProcessor extends FileProcessor
 
         return true;
     }
+
+    /**
+     * @param string $key
+     * @return JsonProcessor
+     * @throws \Exception
+     */
+    public function unsetKey(string $key): JsonProcessor
+    {
+        $value = json_decode($this->fileBuffer, true);
+        $original = &$value;
+        $parts = explode('.', $key);
+        while (count($parts) > 1) {
+            $part = array_shift($parts);
+
+            if (isset($value[$part]) && is_array($value[$part])) {
+                $value = &$value[$part];
+            } else {
+                throw new \Exception('Key not found');
+            }
+        }
+
+        unset($value[array_shift($parts)]);
+
+        $result = json_encode($original, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+        $result = preg_replace('/^(  +?)\\1(?=[^ ])/m', '$1', $result);
+        $result .= "\n";
+
+        $this->fileBuffer = $result;
+        return $this;
+    }
+
+    public function notIsset(string $key): bool
+    {
+        return !$this->isset($key);
+    }
 }
