40d625e388ce4cd1524e9a35c307daf9e60d1af9
Rostislav Khaniukov <rostyslav.khanyukov@ms.sumdu.edu.ua>
2019-11-11 22:48:48 +0200

    asdsa

--- src/EmptyBuilder.php
+++ src/EmptyBuilder.php
@@ -6,6 +6,11 @@ namespace AirSlate\Releaser;
 
 class EmptyBuilder
 {
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return $this
+     */
     public function __call(string $name, array $arguments): self
     {
         return $this;
